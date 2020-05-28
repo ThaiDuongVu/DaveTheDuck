be67d0ed2e6f91b0dc6d89895ceb4f8cdc03b8a0
Thai Duong Vu <vu.t.duong01@gmail.com>
2020-05-27 20:04:40 -0400

    Add player control and texture handling

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private const float speed = 15f;
+
+    private void Start()
+    {
+
+    }
+
+
+    private void Update()
+    {
+        MovementControl();
+    }
+
+    private void MovementControl()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+
+        if (movement != Vector3.zero)
+        {
+            gameObject.transform.rotation = Quaternion.LookRotation(movement);
+        }
+        gameObject.transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+}
---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+
+    private Material startMaterial;
+    public Material paintMaterial;
+
+    private bool painted;
+
+    private void Awake()
+    {
+        meshRenderer = gameObject.transform.GetComponent<MeshRenderer>();
+        startMaterial = meshRenderer.material;
+    }
+
+    private void Start()
+    {
+        painted = false;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (!painted)
+            {
+                SetMaterial(paintMaterial);
+            }
+        }
+    }
+
+    private void SetMaterial(Material material)
+    {
+        meshRenderer.material = material;
+        painted = true;
+    }
+}
