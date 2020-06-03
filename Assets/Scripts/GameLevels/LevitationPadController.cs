f9654e714207c4c4741e75e016813be8bf478f5e
Thai Duong Vu <vu.t.duong01@gmail.com>
2020-06-03 00:37:49 -0400

    Add levitation pad

---
+++ Assets/Scripts/GameLevels/LevitationPadController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class LevitationPadController : MonoBehaviour
+{
+    private Animator animator;
+    private float force = 1300f;
+
+    private void Awake()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+    
+    private void Start()
+    {
+        
+    }
+    
+    private void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * force);
+        }
+    }
+}
